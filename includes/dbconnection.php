bf0a006da868b928c381e964aea7ee5d44d02804
Gbode Abiola <Gbode.Abiola@etranzact.com>
2019-11-08 17:46:32 +0100

    vms

---
+++ includes/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("sql108.epizy.com", "epiz_24747596", "Moses2012", "epiz_24747596_cvms");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
